dbdad228e21e2e675e0ddd26f8b796cded27bd43
Eric Andrews <eric.andrews@streamr.com>
2021-03-03 16:01:54 +0200

    run-clients-in-screen.sh: add prefix name argument

--- repro-buffer-issue/run-clients-in-screens.sh
+++ repro-buffer-issue/run-clients-in-screens.sh
@@ -1,12 +1,20 @@
 
 NUM=10
-WSURL='ws://localhost:8080/'
+WSURL='ws://95.216.64.56:8080/'
+NODE_ID_PREFIX='client'
 
 if [ -z "$1" ]
   then
-    echo "No first argument given, running default of ${NUM} clients"
+    echo "No first argument given, using ${NODE_ID_PREFIX} as default node id prefix"
   else
-    NUM=$1
+    NODE_ID_PREFIX=$1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No second argument given, running default of ${NUM} clients"
+  else
+    NUM=$2
 fi
 
 if [ -z "$WS_URL" ]
@@ -20,7 +28,7 @@ fi
 mkdir -p logs
 for (( c=1; c<=$NUM; c++ ))
 do  
-   screen -S tsclient${c} -dm bash -c 'WS_URL=${WSURL} npx ts-node client.ts > logs/tsclient${c}.txt; exec sh'
+   screen -S tsclient${c} -dm bash -c "WS_URL=${WSURL} npx ts-node client.ts ${NODE_ID_PREFIX}${c} &> logs/${NODE_ID_PREFIX}${c}.txt ; exec sh"
 done
 
 screen -ls
