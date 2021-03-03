c2648657b8a2d18a9aa48114ad199d3f231c85ca
Eric Andrews <eric.andrews@streamr.com>
2021-03-03 15:44:46 +0200

    add log files to run-clients-in-screen.sh

--- repro-buffer-issue/run-clients-in-screens.sh
+++ repro-buffer-issue/run-clients-in-screens.sh
@@ -17,9 +17,10 @@ if [ -z "$WS_URL" ]
 fi
 
 
+mkdir -p logs
 for (( c=1; c<=$NUM; c++ ))
 do  
-   screen -S tsclient${c} -dm bash -c 'WS_URL=${WSURL} npx ts-node client.ts; exec sh'
+   screen -S tsclient${c} -dm bash -c 'WS_URL=${WSURL} npx ts-node client.ts > logs/tsclient${c}.txt; exec sh'
 done
 
 screen -ls
