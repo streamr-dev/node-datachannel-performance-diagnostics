891a66e72e04b7bff204086a752aee1a9c494098
Paul-Louis Ageneau <paul-louis@ageneau.org>
2021-10-01 19:46:33 +0200

    Fixes (#2)
    * Update node-datachannel to ^0.1.10
    
    * Increase TCP buffer size to 64 MB and reduce message size to 65535
    
    * cpp: Remove STUN server and increase SCTP buffer size to 64 MB

--- benchmark/cppbenchmark/peer.cpp
+++ benchmark/cppbenchmark/peer.cpp
@@ -15,21 +15,24 @@ Peer::Peer(string _selfId, string _peerId, shared_ptr<WebSocket> _signalingServe
 	selfId(_selfId), peerId(_peerId), signalingServerWs(_signalingServerWs), packetSize(_packetSize), isSender(_isSender)
 	{
 	this->logId = this->selfId + "->" + this->peerId;
-	string stunServer = "stun:stun.l.google.com:19302";
-	
-    this->message = Helpers::randomString(packetSize);
-        
-    Configuration config;
-	config.iceServers.emplace_back(stunServer);
-	this->connection = make_shared<PeerConnection>(config);
-		
+
+        this->message = Helpers::randomString(packetSize);
+
+        SctpSettings settings = {};
+        settings.recvBufferSize = 64 * 1024 * 1024;
+        settings.sendBufferSize = 64 * 1024 * 1024;
+        SetSctpSettings(std::move(settings));
+
+        Configuration config = {};
+        this->connection = make_shared<PeerConnection>(std::move(config));
+
 	this->connection->onStateChange([this](PeerConnection::State state)
 		{
 		//cout << now() << "State: " << state << endl;
 		this->lastState = state;
 		});
-	
-   
+
+
 	this->connection->onLocalDescription([_signalingServerWs, _selfId, _peerId](Description description)
 		{
 		json data =  {{"id", _selfId}, {"type", description.typeString()}, {"description", string(description)}};
@@ -39,30 +42,30 @@ Peer::Peer(string _selfId, string _peerId, shared_ptr<WebSocket> _signalingServe
 				{"to", _peerId},
 				{"data", data}
 			};
-		
+
 		_signalingServerWs->send(message.dump());
 		});
-	   
+
 	this->connection->onLocalCandidate([_signalingServerWs, _selfId, _peerId](Candidate candidate)
 		{
 		json data = { {"candidate", candidate}, {"mid",candidate.mid()} };
-		
+
 		json message =
 				   {
 					   {"type", "relay"},
 					   {"to", _peerId},
 					   {"data", data}
 				   };
-			   
+
 		_signalingServerWs->send(message.dump());
 		});
    }
 
 void Peer::setUpDataChannel(shared_ptr<DataChannel> dc)
 	{
-	this->dc = dc;    
+	this->dc = dc;
 	dc->setBufferedAmountLowThreshold(BUFFER_LOW);
-		
+
 	dc->onOpen([obj = this, logId = this->logId, peerId = this->peerId]()
 		{
 		cout << now() << logId << " !!!!!!!!!!!!!!!!!!!!!!!!!! DataChannel " << peerId << " open, isSender: "<< obj->isSender << endl;
@@ -71,20 +74,20 @@ void Peer::setUpDataChannel(shared_ptr<DataChannel> dc)
             obj->publish(obj->message);
             }
         });
-		
+
 	dc->onClosed([logId = this->logId, peerId = this->peerId]()
 		{
 		cout << now() << logId << " DataChannel " << peerId << " closed" << endl;
 		});
-		
+
 	dc->onError([logId = this->logId, peerId = this->peerId](string err)
 		{
 		cout << now() << logId << " DataChannel " << peerId << " error: "<< err << endl;
 		});
-		
+
     if (this->isSender)
         {
-            
+
         cout<<"setting onBufferedAmountLow callback"<<endl;
         dc->onBufferedAmountLow([obj = this, logId = this->logId, peerId = this->peerId, dc= this->dc] ()
             {
@@ -92,22 +95,22 @@ void Peer::setUpDataChannel(shared_ptr<DataChannel> dc)
             obj->publish(obj->message);
             });
         }
-    
+
 	dc->onMessage([obj=this] (message_variant msg)
 		{
         //cout << "onMessage" << endl;
 		if (holds_alternative<string>(msg))
 			{
-            
+
             uint64_t size = get<string>(msg).size();
 			uint64_t now = Helpers::millisecondsNow();
-           
-			
+
+
             if (obj->stats.find(size) == obj->stats.end())
                 {
-                
+
                 struct Statistic stat = {now, now, size};
-                
+
                 obj->stats.emplace(size, stat);
                 }
             else
@@ -115,7 +118,7 @@ void Peer::setUpDataChannel(shared_ptr<DataChannel> dc)
                 auto stat = obj->stats.at(size);
                 stat.bytesReceived += size;
                 stat.lastPacketAt = now;
-                
+
                 obj->stats[size] = stat;
                 }
             obj->bytesIn += size;
@@ -131,9 +134,9 @@ void Peer::startAsActive()
 		{
 		throw runtime_error("Already started!");
 		}
-	
+
 	this->dc =  this->connection->createDataChannel("generalDataChannel");
-		
+
 	this->setUpDataChannel(this->dc);
     //if (this->isSender)
      //   {
@@ -152,18 +155,18 @@ void Peer::startAsPassive()
 		{
 		throw runtime_error("Already started!");
 		}
-		
+
 	this->connection->onDataChannel([this](shared_ptr<DataChannel> dc)
 		{
 		cout << now() << "DataChannel received with label \"" << dc->label() << "\"" << endl;
 
 		this->setUpDataChannel(dc);
-        
+
         if (this->isSender)
             {
             this->publish(this->message);
             }
-        
+
         //this->paused = false;
 		});
 	}
@@ -174,36 +177,36 @@ void Peer::handleRemoteData(json data)
 	string mid = "";
 	string description = "";
 	string type = "";
-		
+
 	auto it = data.find("candidate");
 	if (it != data.end())
 		{
 		candidate = it->get<string>();
 		}
-   
+
 	auto it2 = data.find("mid");
 	if (it2 != data.end())
 		{
 		mid = it2->get<string>();
 		}
-   
+
 	auto it3 = data.find("description");
 	if (it3 != data.end())
 		{
 		description = it3->get<string>();
 		}
-		   
+
 	auto it4 = data.find("type");
 	if (it4 != data.end())
 		{
 		type = it4->get<string>();
 		}
-	
+
 	if (candidate != "" && mid !="")
 		{
 		this->connection->addRemoteCandidate(candidate);
 		}
-	
+
    else if (description != "" && type!= "")
 		{
 		Description desc(description, type);
@@ -224,7 +227,7 @@ bool Peer::publish(const string& message)
 		this->bytesFailed += message.size();
 		return false;
 		}
-		
+
 	if (this->dc->bufferedAmount() >= BUFFER_HIGH)
 		{
 		this->paused = true;
@@ -232,12 +235,12 @@ bool Peer::publish(const string& message)
 		this->bytesFailed += message.size();
 		return false;
 		}
-		
-	try 
+
+	try
 		{
 		this->dc->send(message);
-		} 
-	catch (const std::exception& e) 
+		}
+	catch (const std::exception& e)
 		{
 		cout << now() << this->logId << " send failed: " << e.what() << endl;
 		this->bytesFailed += message.size();
@@ -246,14 +249,14 @@ bool Peer::publish(const string& message)
 	this->bytesOut += message.size();
 	return true;
 	*/
-    
+
     while (this->dc->bufferedAmount() < BUFFER_HIGH)
         {
         try
             {
-           
+
             auto result = this->dc->send(message);
-           
+
             this->bytesOut += message.size();
             }
         catch (const std::exception& e)
@@ -262,7 +265,7 @@ bool Peer::publish(const string& message)
             break;
             }
         }
-        
+
     return true;
     }
 
@@ -312,23 +315,23 @@ string Peer::getStatsAsString()
     {
     cout<<"getStatsAsString()"<<endl;
     string ret = "";
-        
+
     for (auto it = this->stats.begin(); it != this->stats.end(); it++)
         {
         //cout << (*it).first << endl;
         auto stat = (*it).second;
         //cout << stat.lastPacketAt << "-" << stat.fistPacketAt << endl;
         uint64_t timeSpent = stat.lastPacketAt - stat.fistPacketAt;
-        
-            
+
+
         //cout << "timeSpentti: " << to_string(timeSpent) <<endl;
         //cout << "bytesReceived: " << stat.bytesReceived <<endl;
-            
+
         double bytesPerSecond = (static_cast<double>(stat.bytesReceived) / timeSpent) * 1000;
         double kilobytesPerSecond = (bytesPerSecond / 1024);
         double megabitsPerSecond = kilobytesPerSecond * 8 / 1024;
         //cout << kilobytesPerSecond << endl;
-        
+
         ret += to_string((*it).first);
         ret += "    ";
         ret += to_string(kilobytesPerSecond);
@@ -340,7 +343,7 @@ string Peer::getStatsAsString()
         ret += to_string(megabitsPerSecond);
         }
     //cout << "ret: " << ret << endl;
-        
+
     return ret;
     }
 
